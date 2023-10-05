f526ada22d1e388a8472767268ffb2e0b18dea52
Irinapreo <irinapreobrazhenskaya100@gmail.com>
2023-10-05 10:35:01 +0200

    initial commit

---
+++ NewsUploader.cs
@@ -0,0 +1,23 @@
+﻿
+namespace Inheritance;
+
+internal class NewsUploader : User
+{
+    public NewsUploder(string username, string email) : base(username, email)
+    {
+
+    }
+    public bool UploadNews(string title, string content)
+    {
+        if (content.Length > 0)
+        {
+            Console.WriteLine("Uploaded news: ", +title);
+            return true;
+
+        }
+        else
+        {
+            return false;
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,14 @@
+﻿
+using Inheritance;
+
+Console.WriteLine("Hello World");
+
+User user = new User(username:"jenseneducation.i.preo", email:"irina@preo.com");
+
+
+Student strudent = new Student(username: "jenseneducation.k.vikander", email: "kv@email.com", klass: "sys23m");
+
+Teacher teacher = new Teacher(username: "jenseneducation.m.noltorp", email: "manuel@noltorp.com");
+
+teacher.UloadNews(title string,
+teacher.UloadNews(title string,
